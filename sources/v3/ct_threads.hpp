99ace5ebe79937d84c5147500826337d6fb0044c
Jean Pierre Cimalando <jp-dev@gmx.com>
2022-06-20 19:18:36 +0200

    Fix typo [ci skip]

--- sources/v3/ct_threads.hpp
+++ sources/v3/ct_threads.hpp
@@ -4,7 +4,7 @@
 // Provides some limited thread-safety on X11.
 // This problem does not exist on Windows and MacOS platforms.
 //
-// In case the plugin runs without a UI loop, timers amd events are processed
+// In case the plugin runs without a UI loop, timers and events are processed
 // by a custom thread. This thread and the host thread run together with the
 // [main-thread] role, potentially concurrently.
 // The problem is partially mitigated by setting mutex guards at strategic
